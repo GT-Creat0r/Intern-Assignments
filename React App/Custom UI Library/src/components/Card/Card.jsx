2e6da0618afeeb2afa1b395ca6886719507595e9
GT Creat0r <bibekgt49@gmail.com>
2025-10-29 18:55:45 +0545

    updated

--- React App/Custom UI Library/src/components/Card/Card.jsx
+++ React App/Custom UI Library/src/components/Card/Card.jsx
@@ -1,9 +1,12 @@
 import "../Card/Card.css";
 
 const Card = ({ children, variant = "default", className = "", ...props }) => {
-  const classNames = ["card", `card-${variant}`, className]
-    .filter(Boolean)
-    .join(" ");
+  
+  // const classNames = ["card", `card-${variant}`, className]
+  //   .filter(Boolean)
+  //   .join(" ");
+
+  const classNames = `card card-${variant} ${className}`;
 
   return (
     <div className={classNames} {...props}>
