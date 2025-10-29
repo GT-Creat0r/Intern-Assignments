286a85e72471544c6e5c2f21a57d2b6c79be95a4
GT Creat0r <bibekgt49@gmail.com>
2025-10-29 18:56:08 +0545

    updated

--- React App/Custom UI Library/src/App.jsx
+++ React App/Custom UI Library/src/App.jsx
@@ -8,18 +8,59 @@ import NavPage from "./pages/NavPage/NavPage";
 import CardPage from "./pages/CardPage/CardPage";
 import TabsPage from "./pages/TabsPage/TabsPage";
 import AlertPage from "./pages/AlertPage/AlertPage";
+import TablePage from "./pages/TablePage/TablePage";
+import UseLocalStoragePage from "./pages/hooks/useLocalStoragePage";
+import UsePreviousPage from "./pages/hooks/UsePreviousPage";
+import UseDimensionPage from "./pages/hooks/UseDimensionPage";
+import UseOutsideClickPage from "./pages/hooks/UseOutsideClickPage";
+import AccordionPage from "./pages/AccordionPage/AccordionPage";
+import ImageSliderPage from "./pages/ImageSliderPage/ImageSliderPage";
+import SliderPage from "./pages/SliderPage/SliderPage";
 
 const navigation = [
-  { id: "home", label: "Home", path: "/" },
-  { id: "alert", label: "Alert", path: "/alert" },
-  { id: "button", label: "Button", path: "/button" },
-  { id: "card", label: "Card", path: "/card" },
-  { id: "input", label: "Input", path: "/input" },
-  { id: "nav", label: "Nav", path: "/nav" },
-  { id: "tabs", label: "Tabs", path: "/tabs" },
-  { id: "textarea", label: "Textarea", path: "/textarea" },
+  {
+    category: "Components",
+    items: [
+      { id: "home", label: "Home", path: "/" },
+      { id: "accordion", label: "Accordion", path: "/accordion" },
+      { id: "alert", label: "Alert", path: "/alert" },
+      { id: "button", label: "Button", path: "/button" },
+      { id: "card", label: "Card", path: "/card" },
+      { id: "imageSlider", label: "ImageSlider", path: "/imageSlider" },
+      { id: "input", label: "Input", path: "/input" },
+      { id: "nav", label: "Nav", path: "/nav" },
+      { id: "slider", label: "Slider", path: "/slider" },
+      { id: "table", label: "Table", path: "/table" },
+      { id: "tabs", label: "Tabs", path: "/tabs" },
+      { id: "textarea", label: "Textarea", path: "/textarea" },
+    ],
+  },
+  {
+    category: "Hooks",
+    items: [
+      {
+        id: "useLocalStorage",
+        label: "useLocalStorage",
+        path: "/useLocalStorage",
+      },
+      {
+        id: "usePrevious",
+        label: "usePrevious",
+        path: "/usePrevious",
+      },
+      {
+        id: "useDimension",
+        label: "useDimension",
+        path: "/useDimension",
+      },
+      {
+        id: "useOutsideClick",
+        label: "useOutsideClick",
+        path: "/useOutsideClick",
+      },
+    ],
+  },
 ];
-
 const App = () => {
   return (
     <BrowserRouter>
@@ -33,6 +74,14 @@ const App = () => {
           <Route path="/textarea" element={<TextareaPage />} />
           <Route path="/nav" element={<NavPage />} />
           <Route path="/alert" element={<AlertPage />} />
+          <Route path="/table" element={<TablePage />} />
+          <Route path="/accordion" element={<AccordionPage />} />
+          <Route path="/imageSlider" element={<ImageSliderPage />} />
+          <Route path="/slider" element={<SliderPage />} />
+          <Route path="/useLocalStorage" element={<UseLocalStoragePage />} />
+          <Route path="/usePrevious" element={<UsePreviousPage />} />
+          <Route path="/useDimension" element={<UseDimensionPage />} />
+          <Route path="/useOutsideClick" element={<UseOutsideClickPage />} />
         </Routes>
       </DocsLayout>
     </BrowserRouter>
--- React App/Custom UI Library/src/layouts/DocsLayout/DocsLayout.jsx
+++ React App/Custom UI Library/src/layouts/DocsLayout/DocsLayout.jsx
@@ -3,26 +3,32 @@ import "../DocsLayout/DocsLayout.css";
 
 const DocsLayout = ({ children, navigation }) => {
   const location = useLocation();
+
   return (
     <div className="docs-layout">
       <aside className="docs-sidebar">
         <div className="docs-sidebar-header">
-          <h2>UI components</h2>
+          <h2>UI Library</h2>
         </div>
-        <nav className="docs-nav">
-          {navigation.map((item) => (
-            <Link
-              key={item.id}
-              to={item.path}
-              className={`docs-nav-item ${
-                location.pathname === item.path ? "docs-nav-item-active" : ""
-              }`}
-            >
-              {item.label}
-            </Link>
-          ))}
-        </nav>
+
+        {navigation.map((section) => (
+          <nav key={section.category} className="docs-nav">
+            <h3>{section.category}</h3>
+            {section.items.map((item) => (
+              <Link
+                key={item.id}
+                to={item.path}
+                className={`docs-nav-item ${
+                  location.pathname === item.path ? "docs-nav-item-active" : ""
+                }`}
+              >
+                {item.label}
+              </Link>
+            ))}
+          </nav>
+        ))}
       </aside>
+
       <main className="docs-main">{children}</main>
     </div>
   );
